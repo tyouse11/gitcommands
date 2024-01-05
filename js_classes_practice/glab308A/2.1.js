fbf5777d864355ff6e6c460f2d248df8e88b4810
tyouse1 <tyouse1@live.com>
2024-01-05 13:34:41 -0500

    Part 2 created a Character class that defined generic character entities included a constructor function that allows us to create new characters added the roll method to the Character class

--- js_classes_practice/glab308A/2.1.js
+++ js_classes_practice/glab308A/2.1.js
@@ -1,7 +1,7 @@
 //Creating an adventure game
 
 //Part 1: Humble Beginnings
-//model a simple adventurer with basic properties such as health and an inventory
+//model a simple adventurer (object) with basic properties such as health and an inventory
 
 const adventurer = {
     name: "Robin",
@@ -30,5 +30,37 @@ adventurer.roll()
 adventurer.inventory.forEach(item => {
     console.log(item)
 })
+
+//Part 2
+
+// Create a Character class which defines generic character entities...name, health(max 100), and inventory
+// Character Class including a construction function that allows us to create new characters with whater name we would like
+
+class Character {
+    constructor(name) {
+        this.name = name
+        this.health = 100
+        this.inventory = []
+    }
+    // Every character should be able to make rolls. Add the roll method to the Character Class
+    roll (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod
+    console.log(`${this.name} rolled a ${result}.`)
+    }
+}
+
+
+// Re-create Robin using the Character class
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+// Companions rolling the dice
+robin.companion.roll()
+robin.companion.companion.roll()
 
 
